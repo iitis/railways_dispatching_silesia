6018cdd1a6888e93506b8804b70bac4592f0f125
Ludmila Botelho <ludmilaasb@gmail.com>
2021-06-18 14:11:42 +0200

    bug fixed

--- src/make_trains_dict.py
+++ src/make_trains_dict.py
@@ -3,6 +3,23 @@ import numpy as np
 
 data = pd.read_csv("data/train_schedule.csv", sep = ";")
 
+def get_indexes(dfObj, value):
+    ''' Get index positions of value in dataframe i.e. dfObj.'''
+    listOfPos = list()
+    # Get bool dataframe with True at positions where the given value exists
+    result = dfObj.isin([value])
+    # Get list of columns that contains the value
+    seriesObj = result.any()
+    columnNames = list(seriesObj[seriesObj == True].index)
+    # Iterate over list of columns and fetch the rows indexes where value exists
+    for col in columnNames:
+        rows = list(result[col][result[col] == True].index)
+        for row in rows:
+            listOfPos.append((row, col))
+    # Return a list of tuples indicating the positions of value in the dataframe
+    return listOfPos
+
+
 train_dict = {}
 for i in range(len(data['Unnamed: 0'])):
     if data['Unnamed: 0'][i].isnumeric():
@@ -20,4 +37,6 @@ for i in range(len(data['Unnamed: 0'])):
             tt_item = data['Unnamed: 0'][i+pa]
         train_data = data.iloc[tt_init:pa-1+tt_init].reset_index(drop=True).rename(columns={'Unnamed: 0': 'path'})
         info += [data['Unnamed: 0'][pa-1+tt_init]]
-        train_dict[train] = [info,train_data]
+    train_dict[train] = [info,train_data]
+
+path_check_data = pd.read_excel("data/KZ-KO-KL-CB_paths.ods", engine="odf")
