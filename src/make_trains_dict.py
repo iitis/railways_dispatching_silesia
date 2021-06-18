c47dd267662c7a167c8bcc84d1bfe490c853ef08
Ludmila Botelho <ludmilaasb@gmail.com>
2021-06-18 17:02:11 +0200

    up

--- src/make_trains_dict.py
+++ src/make_trains_dict.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("../data/train_schedule.csv", sep = ";")
-
 def get_indexes(dfObj, value):
     ''' Get index positions of value in dataframe i.e. dfObj.'''
     listOfPos = list()
@@ -20,23 +18,27 @@ def get_indexes(dfObj, value):
     return listOfPos
 
 # convert csv to dictionary
-train_dict = {}
-for i in range(len(data['Unnamed: 0'])):
-    if data['Unnamed: 0'][i].isnumeric():
-        info = []
-        train = data['Unnamed: 0'][i]
-        info += [data['Unnamed: 0'][i-1]]
-        info += [data['Unnamed: 0'][i+x] for x in range(1,3)]
-    statment = data['Unnamed: 0'][i][0:4]
-    if statment in ['from','depa']:
-        tt_init = i+1
-        tt_item = data['Unnamed: 0'][i+1]
-        pa = 1
-        while tt_item[0:2] not in ['to','te']:
-            pa+=1
-            tt_item = data['Unnamed: 0'][i+pa]
-        train_data = data.iloc[tt_init:pa-1+tt_init].reset_index(drop=True).rename(columns={'Unnamed: 0': 'path'})
-        info += [data['Unnamed: 0'][pa-1+tt_init]]
-        train_dict[train] = [info,train_data]
-print(train_dict)
+def timetable_to_train_dict():
+    data = pd.read_csv("../data/train_schedule.csv", sep = ";")
+    train_dict = {}
+    for i in range(len(data['Unnamed: 0'])):
+        if data['Unnamed: 0'][i].isnumeric():
+            info = []
+            train = data['Unnamed: 0'][i]
+            info += [data['Unnamed: 0'][i-1]]
+            info += [data['Unnamed: 0'][i+x] for x in range(1,3)]
+        statment = data['Unnamed: 0'][i][0:4]
+        if statment in ['from','depa']:
+            tt_init = i+1
+            tt_item = data['Unnamed: 0'][i+1]
+            pa = 1
+            while tt_item[0:2] not in ['to','te']:
+                pa+=1
+                tt_item = data['Unnamed: 0'][i+pa]
+            train_data = data.iloc[tt_init:pa-1+tt_init].reset_index(drop=True).rename(columns={'Unnamed: 0': 'path'})
+            info += [data['Unnamed: 0'][pa-1+tt_init]]
+            train_dict[train] = [info,train_data]
+    return train_dict
+
+# print(train_dict)
 path_check_data = pd.read_excel("../data/KZ-KO-KL-CB_paths.ods", engine="odf")
