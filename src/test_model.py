0384aeb21ae3135f9ad826f63f60b4062e8235d8
Krzysztof Domino <kdomino@iitis.pl>
2021-08-17 09:53:40 +0200

    some ideas about order variables

--- src/test_model.py
+++ src/test_model.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 # from copy import deepcopy
 import numpy as np
 import pulp as pus
+import itertools
 
 # sys.path.append(os.path.abspath("./QUBO"))
 
@@ -13,22 +14,24 @@ import pulp as pus
 
 # model input
 
+# proposed renumbering trains j_1 -> 0, j_2 -> 1, j_3 -> 2; stations s_1 -> 0, s_2 -> 1
+
 def tau(x = None, train = None, first_station = None, second_station = None):
     print(x, train, first_station, second_station)
     t = -1
-    if x == 'pass' and train == 1 and first_station == 1 and second_station == 2:
+    if x == 'pass' and train == 0 and first_station == 0 and second_station == 1:
         t = 4
-    elif x == 'pass' and train == 2 and first_station == 1 and second_station == 2:
+    elif x == 'pass' and train == 1 and first_station == 0 and second_station == 1:
         t = 8
-    elif x == 'pass' and train == 3 and first_station == 2 and second_station == 1:
+    elif x == 'pass' and train == 2 and first_station == 1 and second_station == 0:
         t = 8
-    elif x == 'blocks' and train == 1 and first_station == 1 and second_station == 2:
+    elif x == 'blocks' and train == 0 and first_station == 0 and second_station == 1:
         t = 2
-    elif x == 'blocks' and train == 2 and first_station == 1 and second_station == 2:
+    elif x == 'blocks' and train == 1 and first_station == 0 and second_station == 1:
         t = 2
-    elif x == 'stop' and train == 1 and first_station == 1:
+    elif x == 'stop' and train == 0 and first_station == 0:
         t = 1
-    elif x == 'stop' and train == 2 and first_station == 2:
+    elif x == 'stop' and train == 1 and first_station == 1:
         t = 1
     elif x == 'res':
         t = 1
@@ -38,29 +41,29 @@ def tau(x = None, train = None, first_station = None, second_station = None):
 def earliest_dep_time(train = None, station = None):
     print(train, station)
     t = -1
-    if train == 1 and station == 1:
+    if train == 0 and station == 0:
         t = 4
-    elif train == 2 and station == 1:
+    elif train == 1 and station == 0:
         t = 1
-    elif train == 3 and station == 2:
+    elif train == 2 and station == 1:
         t = 8
     return t
 
 def penalty_weights(train = None, station = None):
     print(train, station)
     w = 0.
-    if train == 1 and station == 1:
+    if train == 0 and station == 0:
         w = 2.
-    elif train == 2 and station == 1:
+    elif train == 1 and station == 0:
         w = 1.
-    elif train == 3 and station == 2:
+    elif train == 2 and station == 1:
         w = 1.
     return w
 
 
 train_sets = {
-  "J": [1,2,3],
-  "Jd": [[1,2], [3]],
+  "J": [0,1,2],
+  "Jd": [[0,1], [2]],
   "Josingle": [],
   "Jround": dict(),
   "Jtrack": dict(),
@@ -68,16 +71,23 @@ train_sets = {
 }
 
 S = {
-    1: [1,2],
-    2: [1,2],
-    3: [2,1]
+    0: [0,1],
+    1: [0,1],
+    2: [1,0]
 }
 
 not_considered_stations = {
+    0: None,
     1: None,
-    2: None,
-    3: 1
+    2: 0
 }
+
+
+def common_path(S, j, jp):
+
+    Sjjp = [s for s in S[j] if s in S[jp]]
+    # without the last element
+    return Sjjp
 
 # print(tau('pass', 1, 1, 2))
 
@@ -90,35 +100,33 @@ def toy_problem_variables(trains_inds, no_station, d_max):
 
     prob = pus.LpProblem("Trains", pus.LpMinimize)
 
-    y = []
-
-    for i in range(3):
-
-        y.append(pus.LpVariable('y'+str(i), upBound = 5, cat='Integer'))
 
     secondary_delays_var = pus.LpVariable.dicts("Delays", (trains_inds, no_station), 0, d_max, cat='Integer')
 
-    print(y)
 
     for key, value in secondary_delays_var.items():
-        if key == 2:
-            del value[0]
+
+        if not_considered_stations[key] != None:
+            v = not_considered_stations[key]
+
+            del value[v]
 
     print(secondary_delays_var)
-    
+
     train1 = []
     train2 = []
-    
+    no_station = []
+    order_the_same_dir = dict()
+
     # this will be the order variable
-    for j1 in trains_inds:
+    for js in train_sets["Jd"]:
+        for pair in itertools.combinations(js, 2):
 
-            train1.append(j1)
+            train1.append(pair[0])
+            train2.append(pair[1])
+            no_station = common_path(S, pair[0], pair[1])
 
-
-    print(train1)
-
-    order_the_same_dir = pus.LpVariable.dicts(
-        "y", (train1, train2, no_station), 0, 1, cat='Integer')
+    order_the_same_dir = pus.LpVariable.dicts("y", (train1, train2, no_station), 0, 1, cat='Integer')
 
 
     print(order_the_same_dir)
@@ -135,7 +143,7 @@ def toy_problem_variables(trains_inds, no_station, d_max):
     #     for j2 in trains_inds:
     #         for k in range(np.size(no_station)):
     #             prob += min_span_delay[j1][j2[k] + mu*
-    
+
     print(prob)
 
     # minimum_span_var = pus.LpVariables.dict("Min_Span", ())
