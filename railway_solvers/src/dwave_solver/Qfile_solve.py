0ad15a3810410d284d5da6b791b7dcbf3b572505
Akash Kundu <kunduaku339@gmail.com>
2021-09-02 13:51:04 +0200

    annealing file added

--- railway_solvers/src/dwave_solver/Qfile_solve.py
+++ railway_solvers/src/dwave_solver/Qfile_solve.py
@@ -13,11 +13,11 @@ import pickle
 
 
 
-def anneal_solutuon(method = None):
+def anneal_solutuon(method):
 
     if method == 'reroute':
         Q_init = np.load('files/Qfile_r.npz')
-    else:
+    elif method == 'default':
         Q_init = np.load('files/Qfile.npz')
     
     Q = Q_init['Q'].astype(np.float32)
@@ -25,16 +25,11 @@ def anneal_solutuon(method = None):
     qubo, offset = model.to_qubo()
     return qubo
 
-
-############################
 def sim_anneal(method):
 
     s = neal.SimulatedAnnealingSampler()
     sampleset = s.sample_qubo(anneal_solutuon(method), beta_range=(5,100), num_sweeps=4000, num_reads=1000,
                                beta_schedule_type='geometric')
-    # lowest = sampleset.first
-    # matrix = dimod.sampleset.as_samples(lowest.sample)[0][0] 
-    # energy = lowest.energy
     return sampleset
 
 
@@ -49,20 +44,59 @@ def hybrid_anneal(method):
     return sampleset
 
 
-if __name__ == "__main__":
+def annealing_outcome(method, annealing, num_reads = None, annealing_time = None):
+    """method: 'reroute', 'default',
+    annealing: 'simulated', 'hybrid', 'quantum', 
+    num_reads: Number of reads in quantum annealing annealing,
+    annealing_time: Choose an annealing time
+    
+    returns: minimum energy/optimal solution"""
+    
+    #simulated annealing!!!!
+    if annealing == 'simulated':
 
-    annealing = 'hybrid'
-    num_reads = 3996
-    annealing_time = 250
-
-    for method in [None]:
+        sampleset = sim_anneal(method)
         
-        #simulated annealing!!!!
-        if annealing == 'simulated':
-            if method == 'reroute':
-                sampleset = sim_anneal('reroute')
-            else:
-                sampleset = sim_anneal(None)
+        results=[]
+        for datum in sampleset.data():
+            x = dimod.sampleset.as_samples(datum.sample)[0][0]
+            results.append((x, datum.energy))
+
+        sdf = sampleset.to_serializable()
+
+        with open("files/Qfile_complete_sol_sim-anneal_{}".format(method), 'wb') as handle:
+            pickle.dump(sdf, handle)
+        with open("files/Qfile_samples_sol_sim-anneal_{}".format(method), 'wb') as handle:
+            pickle.dump(results, handle)
+        
+        print('Simulated solver energy {}'.format(results[0]))
+
+    
+    #hybrid annealing!!!
+    elif annealing == 'hybrid':
+
+        sampleset = hybrid_anneal(method)
+        
+        results = []
+        for datum in sampleset.data():
+            x = dimod.sampleset.as_samples(datum.sample)[0][0]
+            results.append((x, datum.energy))
+
+        sdf = sampleset.to_serializable()
+
+        with open("files/hybrid_data/Qfile_complete_sol_hybrid-anneal_{}".format(method), 'wb') as handle:
+            pickle.dump(sdf, handle)
+        with open("files/hybrid_data/Qfile_samples_sol_hybrid-anneal_{}".format(method), 'wb') as handle:
+            pickle.dump(results, handle)
+        
+        # print('Hybrid solver energy {}'.format(results))
+        
+
+    #quantum annealing!!!!!
+    elif annealing == 'quantum':
+        for chain_strength in [3,3.5,4,4.5]:
+        
+            sampleset = real_anneal(method, num_reads, annealing_time, chain_strength)
             
             results=[]
             for datum in sampleset.data():
@@ -70,74 +104,19 @@ if __name__ == "__main__":
                 results.append((x, datum.energy))
 
             sdf = sampleset.to_serializable()
-            if method == 'reroute':
-                with open("files/Qfile_complete_sol_sim-anneal_r", 'wb') as handle:
-                    pickle.dump(sdf, handle)
-                with open("files/Qfile_samples_sol_sim-anneal_r", 'wb') as handle:
-                    pickle.dump(results, handle)
+
+            with open("files/dwave_data/Qfile_complete_sol_real-anneal_numread{}_antime{}_chainst{}_{}".format(num_reads, annealing_time,chain_strength,method), 'wb') as handle:
+                pickle.dump(sdf, handle)
+            with open("files/dwave_data/Qfile_samples_sol_real-anneal_numread{}_antime{}_chainst{}_{}".format(num_reads, annealing_time,chain_strength, method), 'wb') as handle:
+                pickle.dump(results, handle)
         
-            else:
-                with open("files/Qfile_complete_sol_sim-anneal", 'wb') as handle:
-                    pickle.dump(sdf, handle)
-                with open("files/Qfile_samples_sol_sim-anneal", 'wb') as handle:
-                    pickle.dump(results, handle)
-        
-        #hybrid annealing!!!
-        elif annealing == 'hybrid':
-            if method == 'reroute':
-                sampleset = hybrid_anneal('reroute')
-            else:
-                sampleset = hybrid_anneal(None)
-            
-            results = []
-            for datum in sampleset.data():
-                x = dimod.sampleset.as_samples(datum.sample)[0][0]
-                results.append((x, datum.energy))
-
-            sdf = sampleset.to_serializable()
-            if method == 'reroute':
-                with open("files/hybrid_data/Qfile_complete_sol_hybrid-anneal_r", 'wb') as handle:
-                    pickle.dump(sdf, handle)
-                with open("files/hybrid_data/Qfile_samples_sol_hybrid-anneal_r", 'wb') as handle:
-                    pickle.dump(results, handle)
-        
-            else:
-                with open("files/hybrid_data/Qfile_complete_sol_hybrid-anneal", 'wb') as handle:
-                    pickle.dump(sdf, handle)
-                with open("files/hybrid_data/Qfile_samples_sol_hybrid-anneal", 'wb') as handle:
-                    pickle.dump(results, handle)
-            
-            print('Hybrid solver energy {}'.format(results))
-            
 
 
-        #quantum annealing!!!!!
-        elif annealing == 'quantum':
-            for chain_strength in [3,3.5,4,4.5]:
-            
-                if method == 'reroute':
-                    sampleset = real_anneal('reroute', num_reads, annealing_time, chain_strength)
-                else:
-                    sampleset = real_anneal(None, num_reads, annealing_time, chain_strength)
-                
-                results=[]
-                for datum in sampleset.data():
-                    x = dimod.sampleset.as_samples(datum.sample)[0][0]
-                    results.append((x, datum.energy))
+            # print('Energy {} with chain strength {} run'.format(sampleset.first, chain_strength))
 
-                sdf = sampleset.to_serializable()
-                if method == 'reroute':
-                    with open("files/dwave_data/Qfile_complete_sol_real-anneal_numread{}_antime{}_chainst{}_r".format(num_reads, annealing_time,chain_strength), 'wb') as handle:
-                        pickle.dump(sdf, handle)
-                    with open("files/dwave_data/Qfile_samples_sol_real-anneal_numread{}_antime{}_chainst{}_r".format(num_reads, annealing_time,chain_strength), 'wb') as handle:
-                        pickle.dump(results, handle)
-            
-                else:
-                    with open("files/dwave_data/Qfile_complete_sol_real-anneal_numread{}_antime{}_chainst{}".format(num_reads,annealing_time,chain_strength), 'wb') as handle:
-                        pickle.dump(sdf, handle)
-                    with open("files/dwave_data/Qfile_samples_sol_real-anneal_numread{}_antime{}_chainst{}".format(num_reads,annealing_time,chain_strength), 'wb') as handle:
-                        pickle.dump(results, handle)
 
-                print('Energy {} with chain strength {} run'.format(sampleset.first, chain_strength))
-                # print('Embedding:')
-                # print(sampleset.info['embedding_context'])
+if __name__ == "__main__":
+    import sys
+
+    sys.path.append('../files')
+    print(annealing_outcome('reroute','simulated'))
