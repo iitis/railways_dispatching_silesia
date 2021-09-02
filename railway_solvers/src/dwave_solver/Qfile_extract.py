974fe9a714b2bda6ed247c233edc3bda7d578cf2
Akash Kundu <kunduaku339@gmail.com>
2021-09-02 13:30:01 +0200

    up

---
+++ railway_solvers/src/dwave_solver/Qfile_extract.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pickle
+import pandas as pd
+
+
+
+# with open('Qfile_samples_sol_real-anneal_numread1998_antime500_chainst4.5_r', 'rb') as fp:
+    # x = pickle.load(fp)
+    # print(x)
+
+print('Non-Reroute:')
+for j in [3,3.5,4,4.5]:
+    with open('files/dwave_data/Qfile_samples_sol_real-anneal_numread3996_antime250_chainst{}'.format(j), 'rb') as fp:
+        x = pickle.load(fp)
+        print(x[0][1], 'chain strenght', j)
+
+
+print('------------')
+
+print('Reroute:')
+for j in [3,3.5,4,4.5]:
+    with open('files/dwave_data/Qfile_samples_sol_real-anneal_numread3996_antime250_chainst{}_r'.format(j), 'rb') as fp:
+        x = pickle.load(fp)
+        print(x[0][1], 'chain strenght', j)
+
+print()
+print('---------')
+print('Hybrid - non-Reroute:')
+with open('files/hybrid_data/Qfile_samples_sol_hybrid-anneal', 'rb') as fp:
+    x = pickle.load(fp)
+
+print(x[0][1])
+
+print('Hybrid - non-Reroute:')
+with open('files/hybrid_data/Qfile_samples_sol_hybrid-anneal_r', 'rb') as fp:
+    x = pickle.load(fp)
+print(x[0][1])
