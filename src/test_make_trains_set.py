8a0c2818d3ea6fcc467db3611fe64b50b447f589
Ludmila Botelho <ludmilaasb@gmail.com>
2021-12-10 10:51:44 +0100

    test for checking blocks between stations, need assert error

--- src/test_make_trains_set.py
+++ src/test_make_trains_set.py
@@ -1,7 +1,9 @@
 from make_trains_set import *
 
 train1,train2 = 94766,26103
+station1,station2 = 'Ty','KO'
 print('"J":',get_J(data),'\n')
 print('"Paths":',get_Paths(data))
 print('Common station for trains {} and {}:'.format(train1,train2),check_common_station(train1,train2))
 print('Jround',get_jround())
+print('block between stations {} and {} for train {}'.format(station1,station2,train1), get_block_b2win_station4train(train1,station1,station2))
