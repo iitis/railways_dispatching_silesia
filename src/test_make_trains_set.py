cf4b3783a65a1593505872a9812afc959fa3d7dd
Krzysztof Domino <kdomino@iitis.pl>
2021-12-10 11:04:00 +0100

    a problematic test

--- src/test_make_trains_set.py
+++ src/test_make_trains_set.py
@@ -1,7 +1,8 @@
 from make_trains_set import *
 
 train1,train2 = 94766,26103
-station1,station2 = 'Ty','KO'
+train1 = 26103
+station1,station2 = 'KO','CB'
 print('"J":',get_J(data),'\n')
 print('"Paths":',get_Paths(data))
 print('Common station for trains {} and {}:'.format(train1,train2),check_common_station(train1,train2))
