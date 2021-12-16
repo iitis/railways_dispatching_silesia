21e4b3994a8b12a3646784d9c446bed53464dbaf
Özlem Salehi Köken <ozlemsalehi@gmail.com>
2021-12-16 16:04:59 +0300

    add comments for bqm optimization

--- railway_solvers/bqm_optimization.py
+++ railway_solvers/bqm_optimization.py
@@ -1,12 +1,45 @@
+from typing import Tuple
+
 from Qfile_solve import *
-from railway_solvers import convert_to_bqm, store_result, get_results, load_results, get_best_feasible_sample, \
-    create_linear_problem, convert_to_cqm, solve_linear_problem
+from railway_solvers import (
+    convert_to_bqm,
+    store_result,
+    get_results,
+    load_results,
+    get_best_feasible_sample,
+    create_linear_problem,
+    convert_to_cqm,
+)
 import os
 import logging
 import importlib
 
 
-def get_file_name(input_name, method, train_route, num_reads=None, annealing_time=None, chain_strength=None):
+def get_file_name(
+    input_name,
+    method,
+    train_route,
+    num_reads=None,
+    annealing_time=None,
+    chain_strength=None,
+) -> str:
+    """Generates a file name based on the given parameters
+
+    :param input_name: Name of the input data
+    :type input_name: str
+    :param method: method of execution
+    :type method: str
+    :param train_route: Default or rerouted
+    :type train_route: str
+    :param num_reads: Number of reads in QA
+    :type num_reads: int
+    :param annealing_time: Annealing time for QA
+    :type annealing_time: int
+    :param chain_strength: Chain strength for QA
+    :type chain_strength: int
+    :return: name of the file to be stored
+    :rtype: str
+    """
     folder = f"annealing_results\{input_name}"
     if not os.path.exists(folder):
         os.mkdir(folder)
@@ -17,7 +50,22 @@ def get_file_name(input_name, method, train_route, num_reads=None, annealing_tim
 
 
 def log_experiment(folder, file_name, bqm, dict_list):
-    logging.basicConfig(filename=os.path.join(f"annealing_results\{folder}", 'results.log'), level=logging.INFO)
+    """Creates a log file for the experiment to monitor experiment results
+
+    :param folder: Name of the folder to store the log file
+    :type folder: str
+    :param file_name: Name of the log file
+    :type file_name: str
+    :param bqm: Binary quadratic model used in the experiment
+    :type bqm: BinaryQuadraticModel
+    :param dict_list: Analyzed samples sorted according to objective
+    :type dict_list: List[Dict[str,Any]]
+    """
+
+    logging.basicConfig(
+        filename=os.path.join(f"annealing_results\{folder}", "results.log"),
+        level=logging.INFO,
+    )
     logging.info("------------------------new run---------------------")
     logging.info(file_name)
     logging.info(f"Number of variables: {len(bqm.variables)}")
@@ -27,28 +75,45 @@ def log_experiment(folder, file_name, bqm, dict_list):
         logging.info(d)
 
 
-def get_parameters(real_anneal_var_dict):
+def get_parameters(real_anneal_var_dict) -> Tuple[int, int, int]:
+    """Extracts/sets parameters for annealing experiment
+
+    :param real_anneal_var_dict: Parameters for QA experiment
+    :type real_anneal_var_dict: Dict[str, float]
+    :return: Number of reads, annealing_time and chain strength
+    :rtype: Tuple[int, int, int]
+    """
     if real_anneal_var_dict == None:
         num_reads = 1000
         annealing_time = 250
         chain_strength = 4
     else:
-        num_reads = real_anneal_var_dict['num_reads']
-        annealing_time = real_anneal_var_dict['annealing_time']
-        chain_strength = real_anneal_var_dict['chain_strength']
+        num_reads = real_anneal_var_dict["num_reads"]
+        annealing_time = real_anneal_var_dict["annealing_time"]
+        chain_strength = real_anneal_var_dict["chain_strength"]
 
     return num_reads, annealing_time, chain_strength
 
 
-def annealing(prob, method, train_route, input_name, pdict=None, real_anneal_var_dict=None):
-    """ Inputs:==
-        prob: The problem,
-        method: 'sim', 'real', 'hyb', 'cqm'
-        train_route: 'default' , 'rerouted'
-        pdict: Dictionary of penalties
-        real_anneal_var_dict: dictionary containing 'num_reads', 'annealing_time' and 'chain_strength' i.e. real annealing variables
-        Returns:==  Dict of solutions
+def annealing(
+    prob, method, train_route, input_name, pdict=None, real_anneal_var_dict=None
+):
+    """Performs the annealing experiment
+
+    :param prob: The problem instance
+    :type prob: pulp.pulp.LpProblem
+    :param method: 'sim', 'real', 'hyb', 'cqm'
+    :type method: str
+    :param train_route: 'deafault' or 'rerouted'
+    :type train_route: str
+    :param input_name: name of the input data
+    :type input_name: str
+    :param pdict: Dictionary containing penalty values
+    :type pdict: Dict[str, float]
+    :param real_anneal_var_dict: Parameters for QA
+    :type real_anneal_var_dict: Dict[str, float]
     """
+
     assert method in ["sim", "real", "hyb", "cqm"]
     file_name = get_file_name(input_name, method, train_route)
     if method == "cqm":
@@ -56,13 +121,27 @@ def annealing(prob, method, train_route, input_name, pdict=None, real_anneal_var
         sampleset = constrained_solver(cqm)
     else:
         bqm, interpreter = convert_to_bqm(prob, pdict)
-        if method == 'sim':
+        if method == "sim":
             sampleset = sim_anneal(bqm, num_sweeps=1000, num_reads=1000)
-        elif method == 'real':
-            num_reads, annealing_time, chain_strength = get_parameters(real_anneal_var_dict)
-            file_name = get_file_name(input_name, method, train_route, num_reads, annealing_time, chain_strength)
-            sampleset = real_anneal(bqm, num_reads=num_reads, annealing_time=annealing_time, chain_strength=chain_strength)
-        elif method == 'hyb':
+        elif method == "real":
+            num_reads, annealing_time, chain_strength = get_parameters(
+                real_anneal_var_dict
+            )
+            file_name = get_file_name(
+                input_name,
+                method,
+                train_route,
+                num_reads,
+                annealing_time,
+                chain_strength,
+            )
+            sampleset = real_anneal(
+                bqm,
+                num_reads=num_reads,
+                annealing_time=annealing_time,
+                chain_strength=chain_strength,
+            )
+        elif method == "hyb":
             sampleset = hybrid_anneal(bqm)
 
     store_result(file_name, sampleset)
@@ -71,29 +150,64 @@ def annealing(prob, method, train_route, input_name, pdict=None, real_anneal_var
     log_experiment(input_name, file_name, bqm, dict_list)
 
 
-def test_all_files(method, train_route = "default", pdict=None, real_anneal_var=None):
+def test_all_files(method, train_route="default", pdict=None, real_anneal_var=None):
+    """Runs the annealing experiment for the files inside the inputs folder
+
+    :param method: 'sim', 'real', 'hyb', 'cqm'
+    :type method: str
+    :param train_route: 'default' or 'rerouted'
+    :type train_route: str
+    :param pdict: Dictionary containing penalty values
+    :type pdict: Dict[str, float]
+    :param real_anneal_var_dict: Parameters for QA
+    :type real_anneal_var_dict: Dict[str, float]
+
+    """
     for file in os.listdir("inputs"):
         if "init" not in file and "pycach" not in file:
             test_single_file(file[:-3], method, train_route, pdict, real_anneal_var)
 
 
-def test_single_file(file, method, train_route = "default", pdict=None, real_anneal_var=None):
+def test_single_file(
+    file, method, train_route="default", pdict=None, real_anneal_var=None
+):
+    """Runs the annealing experiment for the files inside the inputs folder
+
+    :param file: Name of the input data file
+    :type file: str
+    :param method: 'sim', 'real', 'hyb', 'cqm'
+    :type method: str
+    :param train_route: 'default' or 'rerouted'
+    :type train_route: str
+    :param pdict: Dictionary containing penalty values
+    :type pdict: Dict[str, float]
+    :param real_anneal_var_dict: Parameters for QA
+    :type real_anneal_var_dict: Dict[str, float]
+    """
     file_name = f"inputs.{file}"
     mdl = importlib.import_module(file_name)
     globals().update(mdl.__dict__)
-    if train_route == 'default':
+    if train_route == "default":
         prob = create_linear_problem(train_sets, timetable, d_max)
     elif train_route == "rerouted":
         prob = create_linear_problem(train_sets_rerouted, timetable, d_max)
-    annealing(prob, method, train_route, file_name, pdict)
+    annealing(prob, method, train_route, file_name, pdict, real_anneal_var)
 
 
 if __name__ == "__main__":
     file = "linear_solver"
     file = "5_trains_all_cases"
     real_anneal_var = {"num_reads": 1000, "annealing_time": 20, "chain_strength": 4}
-    method = 'sim'
-    pdict = {"minimal_span": 2.5, "single_line": 2.5, "minimal_stay": 2.5, "track_occupation": 2.5, "switch": 2.5,
-             "occupation": 2.5, "circulation": 2.5, "objective": 1}
-    test_single_file(file, method, pdict = pdict)
-    #test_all_files(method, pdict = pdict) logging not working properly
+    method = "sim"
+    pdict = {
+        "minimal_span": 2.5,
+        "single_line": 2.5,
+        "minimal_stay": 2.5,
+        "track_occupation": 2.5,
+        "switch": 2.5,
+        "occupation": 2.5,
+        "circulation": 2.5,
+        "objective": 1,
+    }
+    test_single_file(file, method, pdict=pdict)
+    # test_all_files(method, pdict = pdict) logging not working properly
