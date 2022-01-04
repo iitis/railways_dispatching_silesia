843c386adb01451203a784f867fcdc541903f842
Akash Kundu <kunduaku339@gmail.com>
2022-01-04 13:10:59 +0100

    josingle function working

--- src/make_trains_set.py
+++ src/make_trains_set.py
@@ -1,62 +1,65 @@
-from re import VERBOSE
 from time_table_check import *
 import pandas as pd
 from utils import *
 
-def josingle():
+def josingle(imp_stations = None):
 
-    josingle = {}
+    init_josingle = {}
+
+    if imp_stations != None:
+        imp_stations_list = imp_stations
+    else:
+        imp_stations_list = get_all_important_station()
+
 
     trains_at_stations = get_trains_depart_from_station()
 
-    for s in get_all_important_station():
+    # print(imp_stations_list)
 
+    for s in imp_stations_list:
+        
         for j in trains_at_stations[s]:
-
+            
             s_prime = subsequent_station(j, s)
-
+            
             if s_prime != None:
-
+                
                 for j_prime in trains_at_stations[s]:
-
+                    
                     if j_prime != j:
-
+                        
                         s = subsequent_station(j_prime, s_prime)
-    #                     print(f'{j} goes {s}->{s_prime} while {j_prime} goes {s_prime}->{s}')
 
-                        if s != None:
+                        if (s_prime , s) in init_josingle.keys():
+                            print(s_prime, s)
+                            print('the pair has already been added in the previous steps')
+                        
+                        else:
+                        # print(f'{j} goes {s}->{s_prime} while {j_prime} goes {s_prime}->{s}')
 
-                            path = get_blocks_b2win_station4train(j, s, s_prime, verbose = False)[0]
-                            path_j_prime = get_blocks_b2win_station4train(j_prime, s_prime, s, verbose = False)[0]
+                            if s != None:
 
-                            if len(path) != 0 and path == list(reversed(path_j_prime)):
+                                path = get_blocks_b2win_station4train(j, s, s_prime, verbose = False)[0]
+                                path_j_prime = get_blocks_b2win_station4train(j_prime, s_prime, s, verbose = False)[0]
 
-                                if (s,s_prime) or (s_prime,s) not in josingle.keys():
+                                if len(path) != 0 and path == list(reversed(path_j_prime)):
+                                    
+                                    if (s,s_prime) in init_josingle.keys():
+                                        init_josingle[(s, s_prime)].append([j, j_prime])
+                                    else:
+                                        init_josingle[(s, s_prime)] = [[j, j_prime]]
 
-                                    josingle[(s,s_prime)] = [j, j_prime]
+                                elif len(list(set(path).intersection(path_j_prime))) != 0:
 
-                                else:
-
-                                    josingle[(s,s_prime)].append([j, j_prime])
+                                    print('assertion error partial common path is not supported')
 
 
-                            elif len(list(set(path).intersection(path_j_prime))) != 0:
-
-                                print('assertion error "partial common path is not supported')
-
-    return josingle
+    return init_josingle
 
 
 if __name__ == "__main__":
 
     data = pd.read_csv("../data/train_schedule.csv", sep = ";")
-
+    
+    imp_stations = ['Mi', 'MJ', 'KL']
     print(josingle())
-    # k = {('1', '2'):[3, 1], ('2', '1'): [1, 3]}
-
-    # for keys1 in k.keys():
-    #     for keys2 in k.keys():
-
-    #         if keys1 != keys2:
-
-    #             print(keys1, keys2)
