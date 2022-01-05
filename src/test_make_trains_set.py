73deab9c752543f6a43bb9dd5860629d0e498705
Krzysztof Domino <kdomino@iitis.pl>
2022-01-05 14:04:59 +0100

    correction

--- src/test_make_trains_set.py
+++ src/test_make_trains_set.py
@@ -17,4 +17,5 @@ print(f'\nblock between stations {station1} and {station2} for train {train1}', 
 print(f'\ncommon blocks between stations {station1} and {station2} for train {train1} and train {train2}', get_common_blocks_and_direction_b2win_trains(data,train1, train2,station1,station2))
 print(f'\ntrains departuring from station {station1}',get_trains_depart_from_station(data)[station1])
 print(f'\nbetween stations {station1} and {station2} the following trains shares a single track while going in opposite direction as follows:')
+
 print(josingle(data, imp_stations))
