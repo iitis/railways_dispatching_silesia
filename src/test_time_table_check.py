6fcff806544dc5efc15c63c7c2685a7dae004fc4
Ludmila Botelho <ludmilaasb@gmail.com>
2022-01-21 12:46:55 +0100

    bug fixed: missing data argument

--- src/test_time_table_check.py
+++ src/test_time_table_check.py
@@ -20,22 +20,22 @@ else:
     print('The available trains are:', *trains_list)
     exit()
 
-schemes,station_ind = get_schmes(train,return_index = True)
-arr_dep_vals = get_arr_dep_vals(train)
+schemes,station_ind = get_schmes(data,train,return_index = True)
+arr_dep_vals = get_arr_dep_vals(data,train)
 cumulative_time = 0
 for i in range(len(schemes)):
-    print('Checking time for path {} to {}'.format(train_time_table(train)['path'][schemes[i][0]],train_time_table(train)['path'][schemes[i][-1]+1]))
+    print('Checking time for path {} to {}'.format(train_time_table(data,train)['path'][schemes[i][0]],train_time_table(data,train)['path'][schemes[i][-1]+1]))
     station_time = 'N/A'
     arr_dep_time = 'N/A'
     check_start = 0
-    total_time,times = check_path_time(train,schemes[i])
+    total_time,times = check_path_time(train,data,schemes[i])
     if len(list(set(schemes[i]).intersection(station_ind))) !=0:
         station_time = times[0][-1]
         value = list((set(schemes[i]).intersection(station_ind)))[0]
-        arr_dep_time = get_arrdep(train).loc[value].dropna().tolist()
+        arr_dep_time = get_arrdep(data,train).loc[value].dropna().tolist()
         check_start=+1
-        if pd.isnull(get_arrdep(train).loc[value]['Approx_enter'])== False:
-            print('Approximate enter',get_arrdep(train).loc[value]['Approx_enter'])
+        if pd.isnull(get_arrdep(data,train).loc[value]['Approx_enter'])== False:
+            print('Approximate enter',get_arrdep(data,train).loc[value]['Approx_enter'])
         else:
             print('Arrival and departure times:',arr_dep_time)
 
@@ -45,11 +45,11 @@ for i in range(len(schemes)):
     print('Station stay time:{},'.format(station_time),'blocks passing time: {}'.format(np.round(blocks_time,1)),"Total time is:",np.round(total_time,1),'cumulative time is:',np.round(cumulative_time,1),'\n')
     # print('For each block {}'.format(times),'\n')
 if len(arr_dep_vals) - len(schemes) == 1:
-    print('The last station {}: {}'.format(train_time_table(train)['path'][schemes[-1][-1]],arr_dep_vals[-1]))
+    print('The last station {}: {}'.format(train_time_table(data,train)['path'][schemes[-1][-1]],arr_dep_vals[-1]))
 
-total_time_path,_ = check_path_time(train,scheme=range(schemes[0][0],schemes[-1][-1]+1),show_warning = False)
+total_time_path,_ = check_path_time(train,data,scheme=range(schemes[0][0],schemes[-1][-1]+1),show_warning = False)
 print("The total time for whole path is {}".format(np.round(total_time_path,1)))
-print("The important stations are:",check_important_stations(train))
+print("The important stations are:",check_important_stations(data,train))
     # for train in list(train_dict.keys()):
     #     total_time,times = check_path_time(train)
     #     print("Total time is:",total_time)
