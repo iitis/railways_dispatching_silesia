810916d4d6beda8065ee346729f8fad149aa3f03
Akash Kundu <kunduaku339@gmail.com>
2022-01-21 16:07:22 +0100

    test for makes_trains_set update

--- src/test_make_trains_set.py
+++ src/test_make_trains_set.py
@@ -1,3 +1,4 @@
+from tkinter.tix import Tree
 from utils import *
 from make_trains_set import *
 
@@ -15,7 +16,7 @@ print('\nJround', get_jround(data))
 print(f'\nblock between stations {station1} and {station2} for train {train1}', get_blocks_b2win_station4train(data, train1, station1, station2))
 # the last is not yet working (now working)
 print(f'\ncommon blocks between stations {station1} and {station2} for train {train1} and train {train2}', get_common_blocks_and_direction_b2win_trains(data,train1, train2,station1,station2))
-print(f'\ntrains departuring from station {station1}',get_trains_depart_from_station(data)[station1])
+print(f'\ntrains departuring from station {station1}', get_trains_at_station(data, True)[station1])
 print(f'\nbetween stations {imp_stations} the following trains shares a single track while going in opposite direction as follows:')
 
 print(josingle(data, imp_stations))
