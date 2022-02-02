f6c6ae27068bc39891b4b25007255d5f57702059
Akash Kundu <kunduaku339@gmail.com>
2022-02-02 12:25:26 +0100

    jswitchupdated

--- src/test_jswitch.py
+++ src/test_jswitch.py
@@ -7,6 +7,7 @@ data_path_check = pd.read_excel("../data/KZ-KO-KL-CB_paths.ods", engine="odf")
 data = pd.read_csv(path_to_data, sep = ";")
 
 
+
 def block_indices_to_interprete_switches(previous_block, next_block):
 
     i = 0
@@ -37,24 +38,21 @@ def z_in(data, j, s):
     in_station = s
     station_block = blocks_list_4station(data, train, in_station)
     blocks_list = train_time_table(data, train)['path'].tolist()
-    sts = get_Paths(data)[train]
 
     for block_list_el_no, block_list_el in enumerate(blocks_list):
 
         if block_list_el == station_block[0]:
             previous_block = blocks_list[block_list_el_no-1]
-            if sts.index(in_station)==len(sts)-1:
-                return []
-            else:
-                next_block = blocks_list[block_list_el_no]
+            next_block = blocks_list[block_list_el_no]
 
             list_pos_previous_block, list_pos_next_block = block_indices_to_interprete_switches(previous_block, next_block)
             switch_position = common_elements(list_pos_next_block, list_pos_previous_block)
 
             for s in switch_position:
                 switch = data_path_check['switches'][s]
+                switch = eval(str(switch).replace('.', ','))
 
-    return switch
+    return list(switch)
 
 
 
@@ -75,12 +73,14 @@ def z_out(data, j, s):
                 next_block = blocks_list[block_list_el_no+1]
 
             list_pos_previous_block, list_pos_next_block = block_indices_to_interprete_switches(previous_block, next_block)
-
+            # print(list_pos_next_block, list_pos_previous_block)
             switch_position = common_elements(list_pos_next_block, list_pos_previous_block)
             for s in switch_position:
                 switch =  data_path_check['switches'][s]
+                switch = eval(str(switch).replace('.', ','))
+                
 
-    return switch
+    return list(switch)
 
 
 if __name__ == "__main__":
