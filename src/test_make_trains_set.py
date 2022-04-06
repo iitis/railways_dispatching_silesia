7e4a69f41fcd93769ae04ef45af3c7ff7a873afa
Krzysztof Domino <kdomino@iitis.pl>
2022-04-06 14:59:00 +0200

    unit tests in Js

--- src/test_make_trains_set.py
+++ src/test_make_trains_set.py
@@ -3,7 +3,7 @@ from utils import *
 from make_trains_set import *
 
 
-def test_Jhelpers(data, data_switch):
+def test_helpers(data, data_switch):
 
     # vector of all trains
     assert get_J(data) == [94766, 26103, 421009, 42100, 5312, 40518, 34319, 343199, 14006, 94611, 40150, 41004, 94113, 40673, 54101, 541019, 40477, 4500, 94317, 44717, 64350, 94717, 44862, 40628, 40675, 73000, 4120]
@@ -32,16 +32,10 @@ def test_Jhelpers(data, data_switch):
     # trains departuring from particular station
     assert get_trains_at_station(data, True)["Mi"] == [44717, 44862]
 
-    print("J helpers tested")
+    print("helpers tested")
 
 
-if __name__ == "__main__":
-
-    data = pd.read_csv("../data/train_schedule.csv", sep = ";")
-    data_switch = pd.read_excel("../data/KZ-KO-KL-CB_paths.ods", engine="odf")
-
-    test_Jhelpers(data, data_switch)
-
+def test_Js(data, data_switch):
 
     #######    particulat Js #####
 
@@ -51,14 +45,48 @@ if __name__ == "__main__":
     # single track occupation
     assert josingle(data, ["CM", "CB"]) == {}
     assert josingle(data, ["Mi", "MJ"]) == {('Mi', 'MJ'): [[44862, 44717]]}
+
+    assert josingle(data, ['KL', 'MJ', 'Mi']) == {('KL', 'MJ'): [[44717, 44862]], ('MJ', 'Mi'): [[44717, 44862]]}
+
+    assert josingle(data, ['GLC', 'CB']) ==  {}
+
+    assert josingle(data, ['CM', 'CB']) ==  {}
     # specific dict format
 
-    #print(f'\nbetween stations {imp_stations_s} the following trains shares a single track while going in opposite direction:')
-    #print('\nJsingle ', josingle(data, imp_stations_s))
+    trains_same_station_block = jtrack(data)
+    assert trains_same_station_block["KZ"] == [[26103, 14006, 40673, 54101, 40675], [42100, 40150, 41004, 4500, 40628, 4120], [5312, 64350, 73000], [34319, 94317]]
+    assert trains_same_station_block["KO"] == [[94766, 40518, 34319, 343199, 64350, 44862], [26103, 40673, 40477, 94317], [421009, 42100, 4500, 40628], [5312, 40150, 73000], [14006, 54101, 541019, 40675], [94611, 94113, 44717, 94717], [41004, 4120]]
+    assert trains_same_station_block["KO(STM)"] == [[26103, 14006, 40673, 54101, 40675], [421009, 42100, 5312, 34319, 40150, 4500, 40628, 73000, 4120], [41004, 64350], [541019, 94317]]
+    assert trains_same_station_block["CB"] == [[26103, 40673, 40675], [5312, 40150, 4500, 40628, 73000], [94317], [64350]]
+    assert trains_same_station_block["GLC"] == [[26103], [5312], [40150, 40673, 40628, 40675], [4500], [73000]]
+    assert trains_same_station_block["KL"] == [[94766, 40518, 41004, 44862, 4120], [14006, 94611, 94113, 40477, 94717], [44717]]
+    assert trains_same_station_block["Mi"] == [[44717], [44862]]
+    assert trains_same_station_block["MJ"] == [[44717], [44862]]
+    assert trains_same_station_block["CM"] == [[94317], [64350]]
 
-    print(f'\nthe lists of trains that occupy the same station block:')
-    print('\nJtrack ', jtrack(data))
+    switches = jswitch(data, data_switch, ["GLC", "MJ", "Mi", "CB", "KO(IC)", "KO(KS)"])
 
-    imp_stations = ["KO(STM)"]
-    print(f'\nfor important station {imp_stations} lists of trains that occupy the same switches:')
-    print('\nJswitch ', jswitch(data, data_switch, imp_stations))
+    assert switches["GLC"] == [{26103: 'in', 40673: 'in'}, {26103: 'in', 40675: 'in'}, {5312: 'out', 40150: 'out'}, {5312: 'out', 4500: 'out'}, {5312: 'out', 40628: 'out'}, {5312: 'out', 73000: 'out'}, {40150: 'out', 4500: 'out'}, {40150: 'out', 40628: 'out'}, {40150: 'out', 73000: 'out'}, {40673: 'in', 40675: 'in'}, {4500: 'out', 40628: 'out'}, {4500: 'out', 73000: 'out'}, {40628: 'out', 73000: 'out'}]
+
+    assert switches['MJ'] == [{44717: 'in', 44862: 'out'}, {44717: 'out', 44862: 'in'}]
+
+    assert switches['Mi'] == [{44717: 'in', 44862: 'out'}]
+
+    assert switches['CB'] == [{26103: 'in', 40673: 'in'}, {26103: 'out', 40673: 'out'}, {26103: 'in', 94317: 'in'}, {26103: 'in', 40675: 'in'}, {26103: 'out', 40675: 'out'}, {5312: 'in', 40150: 'in'}, {5312: 'out', 40150: 'out'}, {5312: 'in', 4500: 'in'}, {5312: 'out', 4500: 'out'}, {5312: 'out', 94317: 'in'}, {5312: 'out', 64350: 'out'}, {5312: 'in', 40628: 'in'}, {5312: 'out', 40628: 'out'}, {5312: 'in', 73000: 'in'}, {5312: 'out', 73000: 'out'}, {40150: 'in', 4500: 'in'}, {40150: 'out', 4500: 'out'}, {40150: 'out', 94317: 'in'}, {40150: 'out', 64350: 'out'}, {40150: 'in', 40628: 'in'}, {40150: 'out', 40628: 'out'}, {40150: 'in', 73000: 'in'}, {40150: 'out', 73000: 'out'}, {40673: 'in', 94317: 'in'}, {40673: 'in', 40675: 'in'}, {40673: 'out', 40675: 'out'}, {4500: 'out', 94317: 'in'}, {4500: 'out', 64350: 'out'}, {4500: 'in', 40628: 'in'}, {4500: 'out', 40628: 'out'}, {4500: 'in', 73000: 'in'}, {4500: 'out', 73000: 'out'}, {94317: 'in', 40628: 'out'}, {94317: 'in', 40675: 'in'}, {94317: 'in', 73000: 'out'}, {64350: 'out', 40628: 'out'}, {64350: 'out', 73000: 'out'}, {40628: 'in', 73000: 'in'}, {40628: 'out', 73000: 'out'}]
+
+    assert switches['KO(IC)'] == []
+
+    assert switches['KO(KS)'] == []
+
+    ####   Jd   have to be encoded   ########
+
+
+
+if __name__ == "__main__":
+
+    data = pd.read_csv("../data/train_schedule.csv", sep = ";")
+    data_switch = pd.read_excel("../data/KZ-KO-KL-CB_paths.ods", engine="odf")
+
+    test_helpers(data, data_switch)
+
+    test_Js(data, data_switch)
