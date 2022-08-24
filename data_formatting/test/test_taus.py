e0d6841e7a8e08c10b2ad702757e03ea2e2a8ea1
Ludmila Botelho <ludmilaasb@gmail.com>
2022-08-24 16:36:04 +0200

    added test for taus_headway

--- data_formatting/test/test_taus.py
+++ data_formatting/test/test_taus.py
@@ -3,7 +3,8 @@
 """
 import pandas as pd
 import pytest
-from data_formatting import get_taus_pass, get_taus_stop, get_taus_prep
+from data_formatting import get_taus_pass, get_taus_stop, get_taus_prep, get_taus_headway
+
 
 
 data = pd.read_csv("../data/train_schedule.csv", sep = ";")
@@ -11,14 +12,15 @@ data_path_check = pd.read_excel("../data/KZ-KO-KL-CB_paths.ods", engine="odf")
 
 
 
-test_pass = {"94766_Ty_KL": 8, "94766_KL_KO": 6, "94766_KO_KO(STM)": 0,
+test_pass = {"94766_Ty_KL": 8, "94766_KL_KO": 5, "94766_KO_KO(STM)": 0,
      "26103_KZ_KO(STM)": 1, "26103_KO(STM)_KO": 0, "26103_KO_CB": 3,
      "26103_CB_GLC": 14, "421009_KO(IC)_KO(STM)": 0, "421009_KO(STM)_KO": 0,
      "42100_KO_KO(STM)": 0, "42100_KO(STM)_KZ": 1}
 
+taus_pass = get_taus_pass(data,data_path_check)
+
 @pytest.mark.parametrize("key, output",list(test_pass.items()))
 def test_taus_pass(key,output):
-    taus_pass = get_taus_pass(data,data_path_check)
     assert taus_pass[f"{key}"] == output
 
 
@@ -27,9 +29,10 @@ test_stop =  {"94766_Ty": 2, "94766_KL": 1, "94766_KO": 3,
                 "26103_CB": 2, '26103_GLC': 1, "421009_KO(STM)": 2,
                 "42100_KO": 8, "42100_KO(STM)": 1, "42100_KZ": 1}
 
+taus_stop = get_taus_stop(data,data_path_check)
+
 @pytest.mark.parametrize("key, output",list(test_stop.items()))
 def test_taus_stop(key,output):
-    taus_stop = get_taus_stop(data,data_path_check)
     assert taus_stop[key]==output
 
 
@@ -38,3 +41,13 @@ def test_taus_prep():
     taus_prep = get_taus_prep(data)
     print(taus_prep)
     assert taus_prep == prep
+
+test_headway = {"26103_14006_KZ_KO(STM)": 1, "26103_40673_KZ_KO(STM)": 1, 
+                  "26103_54101_KZ_KO(STM)": 1 , "26103_40675_KZ_KO(STM)": 1, 
+                  "14006_40673_KZ_KO(STM)": 1, '14006_54101_KZ_KO(STM)': 1, 
+                  "14006_40675_KZ_KO(STM)": 1} 
+
+taus_headway = get_taus_headway(data,data_path_check)
+@pytest.mark.parametrize("key, output",list(test_headway.items()))
+def test_taus_headway(key,output):
+    assert taus_headway[f"{key}"] == output
