857f9d5a74964a7cac849bb2f0b121db8ec82562
Ludmila Botelho <ludmilaasb@gmail.com>
2022-10-07 11:33:18 +0200

    added argparse, under development

--- solve_real_problem.py
+++ solve_real_problem.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
+import argparse
+import sys
+
 from railway_solvers.railway_solvers import delay_varibles, order_variables, solve_linear_problem
 from railway_solvers.railway_solvers import create_linear_problem, delay_and_acctual_time
 from railway_solvers.railway_solvers import  impact_to_objective
@@ -10,10 +13,60 @@ from data_formatting.data_formatting import jd
 from data_formatting.data_formatting import update_all_timetables
 from data_formatting.data_formatting import timetable_to_train_dict
 
-# TODO we should have a path to input file as an agriemnt
+# TODO we should have a path to input file as an argument
 
 # TODO please produce Js and \taus from in the form analogical to, all functions should be in data_formatting.data_formatting
 
+parser = argparse.ArgumentParser()
+# parser.add_argument(
+#     '--debug',
+#     action='store_true',
+#     help='Print debug info'
+# )
+subparsers = parser.add_subparsers(dest='command')
+blame = subparsers.add_parser('blame', help='blame people')
+blame.add_argument(
+    '--dry-run',
+    help='do not blame, just pretend',
+    action='store_true'
+)
+blame.add_argument('name', nargs='+', help='name(s) to blame')
+praise = subparsers.add_parser('praise', help='praise someone')
+praise.add_argument('name', help='name of person to praise')
+praise.add_argument(
+    'reason',
+    help='what to praise for (optional)',
+    default="no reason",
+    nargs='?'
+)
+args = parser.parse_args(command_line)
+if args.debug:
+    print("debug: " + str(args))
+if args.command == 'blame':
+    if args.dry_run:
+        print("Not for real")
+    print("blaming " + ", ".join(args.name))
+elif args.command == 'praise':
+    print('praising ' + args.name + ' for ' + args.reason)
+
+
+
+
+
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--trains_dict", type=str, required=False)
+
+# parser.add_argument("--data", type=str, required=False)
+# parser.add_argument("--data_paths", type=str, required=False)
+# parser.add_argument("--important_stations", type=str, required=False)
+
+# args = parser.parse_args()
+
+print(args)
+# data = arg.data
+# data_path = arg.data_path
+# important_stations = arg.important_stations
 """
     taus = {
         "pass": {
